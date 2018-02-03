cc7a4fc766e9d59f2b818c8d44a6c19e98f174ad
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-02-03 20:11:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function logWhisper(string) {
   console.log("hello")
 }
 function sayHiToGrandma(string) {
-  if () {
+  if ( ===) {
     console.log("Refilling food bowl.")
   } else if (dog === "thirsty") {
     console.log("Refilling water bowl.")
